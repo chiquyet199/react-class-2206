154cb20f9871a0fb00fc7ad71088b53535f8bb3c
Quyết Nguyễn <quyet@lingio.co>
2019-07-06 16:08:10 +0700

    note

--- class4/index.js
+++ class4/index.js
@@ -2,18 +2,18 @@ const showIphoneBtn = document.querySelector('.showIphone')
 const showAndroidBtn = document.querySelector('.showAndroid')
 
 function hideElement(element) {
-  element.classList.add('hide')
+  element.classList.add('hide') //classList là 1 thuộc tính có sẵn của 1 html element
 }
 
 function isIphone(element) {
   let type = element.getAttribute('data-type')
-  return type === 'iPhone'
+  return type === 'iPhone' // biểu thức điều kiện trả về kiểu boolean, có thể gán cho 1 biến, hoặc trả về trực tiếp
 }
 
 function isAndroid(element) {
   let androidTypes = ['samsung', 'xiaomi', 'bphone']
   let type = element.getAttribute('data-type')
-  let result = androidTypes.indexOf(type) !== -1
+  let result = androidTypes.indexOf(type) !== -1 // indexOf là 1 phương thức có sẵn trên mọi Array, nó trả về thứ tự index của phần tử đó trong mảng nếu có. Không có thì sẽ trả về -1
   return result 
 }
 
