d50a737c05baec4f4961a50639c9f05309b42a51
Quyết Nguyễn <quyet@lingio.co>
2019-07-06 15:46:51 +0700

    Handle show android

--- class4/index.js
+++ class4/index.js
@@ -1,4 +1,5 @@
 const showIphoneBtn = document.querySelector('.showIphone')
+const showAndroidBtn = document.querySelector('.showAndroid')
 
 function hideElement(element) {
   element.classList.add('hide')
@@ -10,17 +11,38 @@ function isIphone(element) {
 }
 
 function isAndroid(element) {
+  let androidTypes = ['samsung', 'xiaomi', 'bphone']
   let type = element.getAttribute('data-type')
-  return type === 'android'
+  let result = androidTypes.indexOf(type) !== -1
+  return result 
+}
+
+function showAll(productEls){
+  let i = 0
+  while(i < productEls.length){
+    productEls[i].classList.remove('hide')
+    i++
+  }
 }
 
 showIphoneBtn.addEventListener('click', function() {
   let productEls = Array.from(document.querySelectorAll('.product'))
+  showAll(productEls)
   for (let i = 0; i < productEls.length; i++) {
     let productEl = productEls[i]
     if (!isIphone(productEl)) {
       hideElement(productEl)
     }
   }
+})
 
+showAndroidBtn.addEventListener('click', function() {
+  let productEls = Array.from(document.querySelectorAll('.product'))
+  showAll(productEls)
+  for (let i = 0; i < productEls.length; i++) {
+    let productEl = productEls[i]
+    if (!isAndroid(productEl)) {
+      hideElement(productEl)
+    }
+  }
 })
