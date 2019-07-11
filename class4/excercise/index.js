f87fd1817de974eedca266be86f35ff64d75a94c
Fram <fram@Frams-MacBook-Pro.local>
2019-07-11 20:58:26 +0700

    refactor

--- class4/excercise/index.js
+++ class4/excercise/index.js
@@ -3,13 +3,14 @@ const nameInput = document.querySelector('input[name="phoneName"]')
 const typeInput = document.querySelector('input[name="phoneType"]')
 const priceInput = document.querySelector('input[name="phonePrice"]')
 
-addNewBtn.onclick = onAddClick
+// addNewBtn.onclick = onAddClick
+addNewBtn.addEventListener('click', onAddClick)
 
-function onAddClick(e) {
-  e.preventDefault()
+function onAddClick(event) {
+  event.preventDefault()//Khong co refresh on submit
   const product = {
-    id: new Date().getTime(),
-    name: nameInput.value,
+    id: new Date().getTime(),//tra ve 1 so la miliseconds tinh tu 1-1-1970
+    name: nameInput.value,//Lay gia tri tu input co name="phoneName"
     type: typeInput.value,
     price: priceInput.value,
   }
@@ -23,7 +24,8 @@ function onAddClick(e) {
 }
 
 function isValidProduct(product){
-  return product.name && product.type && product.price
+  let result = product.name  && product.type  && product.price 
+  return result
 }
 
 function clearForm() {
@@ -33,7 +35,11 @@ function clearForm() {
 }
 
 function addNewProduct(product) {
-  const {type, name, price, id} = product
+  const {type, name, price, id} = product // tuong duong voi code o dong 37 - 40
+  // const type = product.type
+  // const name = product.name
+  // const price = product.price
+  // const id= product.id
   const template = `
   <div
     class="product"
@@ -45,13 +51,29 @@ function addNewProduct(product) {
     <span>${price}</span>
   </div>
   `
-  const newProduct = createEl(template)
+  const newProduct = createEl(template)// tao 1 html element tu 1 string
   const contentEl = document.querySelector('.content')
   contentEl.appendChild(newProduct)
 }
 
 function createEl(str) {
-  const div = document.createElement('div')
-  div.innerHTML = str.trim()
-  return div.firstChild
+  const div = document.createElement('div') // tao ra 1 the div
+  /** div = 
+    <div></div>
+   */
+  div.innerHTML = str.trim() //gan innerHTML = str
+  /** div =
+    <div>
+      <div
+        class="product"
+        data-id="${id}"
+        data-price="${price}"
+        data-type="${type}"
+      >
+        <h3>${name}</h3>
+        <span>${price}</span>
+      </div>
+    </div>
+   */
+  return div.firstChild // tra ve firstChild, tra ve div co class product
 }
