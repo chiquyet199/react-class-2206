50b36696b5482f544a21bfe0ab49d5b3cbbcb408
Quyết Nguyễn <quyet@lingio.co>
2019-08-10 17:07:36 +0700

    Push new code

--- react-class-1/src/App.js
+++ react-class-1/src/App.js
@@ -8,35 +8,77 @@ import './App.css'
 class App extends React.Component {
   state = {
     activePage: 'home',
+    products: [
+      {
+        id: 1,
+        name: 'iphone1',
+        os: 'ios',
+        price: 500,
+      },
+      {
+        id: 2,
+        name: 'iphone2',
+        os: 'ios',
+        price: 1000,
+      },
+      {
+        id: 3,
+        name: 'iphone10',
+        os: 'ios',
+        price: 1500,
+      },
+      {
+        id: 4,
+        name: 'iphone11',
+        os: 'ios',
+        price: 2500,
+      },
+    ],
+    shoppingCarts: [],
   }
 
   navigate = page => {
     this.setState({activePage: page})
   }
 
+  renderContent = () => {
+    const {activePage} = this.state
+    switch (activePage) {
+      case 'home':
+        return <Home />
+      case 'products':
+        return <Products />
+      case 'contact':
+        return <Contact />
+      default:
+        return <NotFound navigate={this.navigate} />
+    }
+  }
+
   render() {
     const {activePage} = this.state
-    let Content
-    switch(activePage){
-      case 'home':
-        Content = Home
-        break;
-      case 'products':
-        Content = Products
-        break;
-      case 'contact':
-        Content = Contact
-        break;
-      default:
-        Content = null
-    }
     return (
       <div>
-        <NavBar navigate={this.navigate} activePage={activePage}/>
-        <Content/>
+        <NavBar navigate={this.navigate} activePage={activePage} />
+        {this.renderContent()}
       </div>
     )
   }
+}
+
+function NotFound(props) {
+  return (
+    <div>
+      <h1>PAGE NOT FOUND</h1>
+      <button
+        onClick={() => {
+          props.navigate('home')
+        }}
+      >
+        BACK TO HOME
+      </button>
+    </div>
+  )
 }
 
 export default App
