c3411f1329f9f247029b621c323ce4c761cd25a7
Fram <fram@Frams-MacBook-Pro.local>
2019-08-24 15:13:11 +0700

    Log and demo component life cycle

--- react-class-4/src/components/Products/Products.js
+++ react-class-4/src/components/Products/Products.js
@@ -2,26 +2,51 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {addToCart} from '../../redux/actions'
 
-const Products = props => {
-  return (
-    <div className="page products">
-      <div className="content">
-        {props.products.map(product => (
-          <div key={product.id} className="product">
-            <h3>{product.name}</h3>
-            <span>{product.price}</span>
-            <button
-              onClick={() => {
-                props.addToCart(product)
-              }}
-            >
-              Add to Cart
-            </button>
-          </div>
-        ))}
+class Products extends React.Component {
+  state={counter: 1}
+  componentDidMount(){
+    console.log('Se duoc goi sau khi component duoc render len DOM')
+  }
+  componentDidUpdate(prevProps, prevState, snapshot){
+    console.log('Se duoc goi khi component update xong')
+    console.log('newState', this.state.counter)
+    console.log('oldState', prevState.counter)
+  }
+  componentWillUnmount(){
+    console.log('Se duoc goi khi component chuan bi bi remove khoi DOM')
+  }
+  shouldComponentUpdate(newProps, newState){
+    console.log(this.state.counter)
+    if(newState.counter > 10){
+      return false
+    }
+    return true
+  }
+  render(){
+    const props = this.props
+    return (
+      <div className="page products">
+        <h1>Products</h1>
+        <h2>{this.state.counter}</h2>
+        <button onClick={()=>{this.setState({counter:110})}}>change</button>
+        <div className="content">
+          {props.products.map(product => (
+            <div key={product.id} className="product">
+              <h3>{product.name}</h3>
+              <span>{product.price}</span>
+              <button
+                onClick={() => {
+                  props.addToCart(product)
+                }}
+              >
+                Add to Cart
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 const mapStateToProps = appState => {
